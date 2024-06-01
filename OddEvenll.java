f35555e68d21ae7ba478b41cc22bdcce423b8a72
vishalgaur752 <vishalgaur752@gmail.com>
2024-06-01 17:18:53 +0530

    InstaDSA

---
+++ OddEvenll.java
@@ -0,0 +1,79 @@
+public class OddEvenll {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.print("ll is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static Node oddEven(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+        while (odd.next != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(0);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(4);
+        head.next.next.next.next.next = null;
+        Node result = oddEven(head);
+        while (result != null) {
+            System.out.print(result.data + " ");
+            result = result.next;
+        }
+        System.out.println();
+    }
+}
