d965cc329bb0d5ebbd5dbef1de4612f6974e6ee0
vishalgaur752 <vishalgaur752@gmail.com>
2024-06-01 15:45:59 +0530

    InsatDSA

---
+++ LinkedListCycleeII.java
@@ -0,0 +1,78 @@
+public class LinkedListCycleeII {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empty.");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static Node isCycleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return findNode(head, fast);
+            }
+        }
+        return null;
+    }
+
+    public static Node findNode(Node head, Node fast) {
+        while (head != fast) {
+            head = head.next;
+            fast = fast.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(0);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = head.next.next;
+        Node result = isCycleNode(head);
+        System.out.println(result.data);
+    }
+}
